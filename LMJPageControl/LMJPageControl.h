529a0c0ba37af7c98f897145dc77592dc8e181ee
limingjie <limingjie_mail@163.com>
2016-01-22 10:55:15 +0800

    1.0.0

--- LMJPageControl/LMJPageControl.h
+++ LMJPageControl/LMJPageControl.h
@@ -4,8 +4,12 @@
 //
 //  Version 1.0.0
 //
-//  Created by apple on 14-12-5.
-//  Copyright (c) 2014年 杰睿科技. All rights reserved.
+//  Created by MajorLi on 14-12-5.
+//  Copyright (c) 2014年 iOS开发者公会. All rights reserved.
+//
+//  iOS开发者公会-技术1群 QQ群号：87440292
+//  iOS开发者公会-技术2群 QQ群号：232702419
+//  iOS开发者公会-议事区  QQ群号：413102158
 //
 
 #import <UIKit/UIKit.h>
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # LMJPageControl
-1.0.0
-给定控件宽高，给出两种初始化控件方法：
-1、通过normal/selected图片点初始化控件
-2、通过normal/selected颜色点初始化控件
+1.0.0     
+给定控件宽高，给出两种初始化控件方法：      
+1、通过normal/selected图片点初始化控件     
+2、通过normal/selected颜色点初始化控件      
